dea5b9662db9bc09b07342b6cfc3aa1d740adfa1
SeraphyBR <33378179+SeraphyBR@users.noreply.github.com>
2017-11-26 11:42:24 -0200

    Add files via upload

---
+++ polybar/bluetooth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# Esse script depende do tlp instalado 
+
+
+import subprocess
+
+icon_enabled="  "
+icon_disabled="  "
+
+status = stdoutdata = subprocess.getoutput("bluetooth")
+
+if (status == "bluetooth = on"):
+    print (icon_enabled)
+else:
+    print (icon_disabled)
