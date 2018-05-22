cfe80df4fdad3764dbb4483e051319ea03f1f69d
SeraphyBR <luisjuniorbr@gmail.com>
2018-05-22 17:03:10 -0300

    Update

--- .config/polybar/player-moc.sh
+++ .config/polybar/player-moc.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/bash
 
+# Change according to information you wish to show in your polybar:
+#        %state     State
+#        %file       File
+#        %title     Title
+#        %artist    Artist
+#        %song      SongTitle
+#        %album     Album
+#        %tt        TotalTime
+#        %tl        TimeLeft
+#        %ts        TotalSec
+#        %ct        CurrentTime
+#        %cs        CurrentSec
+#        %b         Bitrate
+#        %r         Rate
+
 if [ "$(mocp -Q %state)" != "STOP" ]
 then
     echo "$(mocp -Q %song) - $(mocp -Q %album)"
