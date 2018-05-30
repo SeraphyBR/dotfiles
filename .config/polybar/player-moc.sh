9d26cc85058513fbaa57e70810a8307831bed4cf
Luiz Junio <luisjuniorbr@gmail.com>
2018-05-30 12:42:28 -0300

    Update player-moc.sh

--- .config/polybar/player-moc.sh
+++ .config/polybar/player-moc.sh
@@ -1,4 +1,19 @@
-#!/usr/bin/bash
+#!/bin/bash
+
+# Change according to information you wish to show in your polybar:
+#        %state     State
+#        %file       File
+#        %title     Title
+#        %artist    Artist
+#        %song      SongTitle
+#        %album     Album
+#        %tt        TotalTime
+#        %tl        TimeLeft
+#        %ts        TotalSec
+#        %ct        CurrentTime
+#        %cs        CurrentSec
+#        %b         Bitrate
+#        %r         Rate
 
 if [ "$(mocp -Q %state)" != "STOP" ]
 then
