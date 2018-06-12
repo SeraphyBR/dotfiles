61435a4740fb81712c5cad605615b2f05867fff0
SeraphyBR <luisjuniorbr@gmail.com>
2018-06-12 18:28:15 -0300

    Update

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,9 +7,9 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-MONITOR=eDP1 polybar seraphybr &
-MONITOR=HDMI1 polybar seraphybr &
-MONITOR=eDP1 polybar seraphybr_bottom &
-MONITOR=HDMI1 polybar seraphybr_bottom &
+MONITOR=eDP1 polybar seraphybr -r &
+MONITOR=HDMI1 polybar seraphybr -r &
+MONITOR=eDP1 polybar seraphybr_bottom -r &
+MONITOR=HDMI1 polybar seraphybr_bottom -r &
 echo "Bars launched..."
 dunstify -u low  "Bars launched"
