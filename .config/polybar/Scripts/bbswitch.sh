5c771e43aa41867c363ced9c94c1e74c033e02b5
SeraphyBR <luisjuniorbr@gmail.com>
2018-06-12 23:09:23 -0300

    Update

--- .config/polybar/Scripts/bbswitch.sh
+++ .config/polybar/Scripts/bbswitch.sh
@@ -2,9 +2,9 @@
 # script criado para ser usado como modulo para o polybar,
 # vai dizer se a placa nvidia está em uso ou não.
 
-    if grep -q "ON" /proc/acpi/bbswitch 
+if grep -q "ON" /proc/acpi/bbswitch 
     then
-      echo "Nvidia %{F#7FFF00}  %{F-}"
-  else
-      echo "Nvidia %{F#7FFF00}  %{F-}"  
-    fi
+    echo "Nvidia %{F#7FFF00}  %{F-}"
+else
+    echo "Nvidia %{F#7FFF00}  %{F-}"  
+fi
