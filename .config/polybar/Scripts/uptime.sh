b61605bf87ec22b50d3edcb0809fdffb1016a9d9
SeraphyBR <luisjuniorbr@gmail.com>
2018-07-16 21:00:29 -0300

    Update

--- .config/polybar/Scripts/uptime.sh
+++ .config/polybar/Scripts/uptime.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-uptime --pretty | sed 's/up //' | sed 's/\ years\?,/y/' | sed 's/\ days\?,/d/' | sed 's/\ hours\?,\?/h/' | sed 's/\ minutes\?/m/'
+uptime --pretty | sed 's/up //; s/\ days\?,/d/; s/\ hours\?,\?/h/; s/\ minutes\?/m/'
