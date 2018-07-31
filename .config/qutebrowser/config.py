22f028da14a2224d051bc31238e8ce780e7d7100
SeraphyBR <luisjuniorbr@gmail.com>
2018-07-31 17:50:31 -0300

    Add qutebroser config file

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,93 @@
+
+
+config.load_autoconfig()
+
+black = "#3f3f3f"
+white = "#ffffff"
+green = "#7ff145"
+red = "#f50f50"
+blue = "#0f91f5"
+purple = "#B981F4"
+orange = "#ffa500"
+base0E = "#26c549"
+
+
+# set qutebrowser colors
+c.colors.completion.category.bg = black
+c.colors.completion.category.border.bottom= black
+c.colors.completion.category.border.top= black
+c.colors.completion.category.fg = blue
+c.colors.completion.fg = white
+c.colors.completion.item.selected.bg = blue
+c.colors.completion.item.selected.border.bottom = blue
+c.colors.completion.item.selected.border.top = blue
+c.colors.completion.item.selected.fg = red
+c.colors.completion.match.fg = green
+c.colors.completion.odd.bg = black
+c.colors.completion.even.bg = black
+c.colors.completion.scrollbar.bg = black
+c.colors.completion.scrollbar.fg = white
+c.colors.downloads.bar.bg = black
+c.colors.downloads.error.fg = green
+c.colors.downloads.start.bg = orange
+c.colors.downloads.start.fg = black
+c.colors.downloads.stop.bg = blue
+c.colors.downloads.stop.fg = black
+c.colors.hints.bg = blue
+c.colors.hints.fg = black
+c.colors.hints.match.fg = white
+c.colors.keyhint.bg = black
+c.colors.keyhint.fg = white
+c.colors.keyhint.suffix.fg = white
+c.colors.messages.error.fg = black
+c.colors.messages.error.bg = green
+c.colors.messages.error.border = green
+c.colors.messages.info.bg = black
+c.colors.messages.info.border = black
+c.colors.messages.info.fg = white
+c.colors.messages.warning.bg = purple
+c.colors.messages.warning.border = purple
+c.colors.messages.warning.fg = black
+c.colors.prompts.bg = black
+c.colors.prompts.border = black
+c.colors.prompts.fg = white
+c.colors.prompts.selected.bg = blue
+c.colors.statusbar.caret.bg = base0E
+c.colors.statusbar.caret.fg = black
+c.colors.statusbar.caret.selection.bg = purple
+c.colors.statusbar.caret.selection.fg = black
+c.colors.statusbar.command.bg = black
+c.colors.statusbar.command.fg = white
+c.colors.statusbar.command.private.bg = black
+c.colors.statusbar.command.private.fg = white
+c.colors.statusbar.insert.bg = purple
+c.colors.statusbar.insert.fg = black
+c.colors.statusbar.normal.bg = black
+c.colors.statusbar.normal.fg = green
+c.colors.statusbar.passthrough.bg = blue
+c.colors.statusbar.passthrough.fg = black
+c.colors.statusbar.private.bg = black
+c.colors.statusbar.private.fg = black
+c.colors.statusbar.progress.bg = purple
+c.colors.statusbar.url.error.fg = green
+c.colors.statusbar.url.fg = white
+c.colors.statusbar.url.hover.fg = white
+c.colors.statusbar.url.success.http.fg = blue
+c.colors.statusbar.url.success.https.fg = green
+c.colors.statusbar.url.warn.fg = orange
+c.colors.tabs.bar.bg = black
+c.colors.tabs.even.bg = black
+c.colors.tabs.even.fg = white
+c.colors.tabs.indicator.error = green
+c.colors.tabs.indicator.start = purple
+c.colors.tabs.indicator.stop = blue
+c.colors.tabs.odd.bg = black
+c.colors.tabs.odd.fg = white
+c.colors.tabs.selected.even.bg = white
+c.colors.tabs.selected.even.fg = black
+c.colors.tabs.selected.odd.bg = white
+c.colors.tabs.selected.odd.fg = black
+# c.colors.webpage.bg = black
+
+
+c.url.start_pages = ["https://www.google.com/"]
