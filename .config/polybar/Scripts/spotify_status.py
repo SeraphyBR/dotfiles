ad6be3b70b95a866ed68fa7f9d8d0554f916f108
SeraphyBR <luisjuniorbr@gmail.com>
2018-11-16 18:08:55 -0200

    Update

--- .config/polybar/Scripts/spotify_status.py
+++ .config/polybar/Scripts/spotify_status.py
@@ -1,10 +1,9 @@
 #!/bin/python 
-# by https://github.com/Jvanrhijn/polybar-spotify 
+# by https://github.com/Jvanrhijn/polybar-spotify
 
 import sys
 import dbus
 import argparse
-
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -20,17 +19,35 @@ parser.add_argument(
     metavar='custom format',
     dest='custom_format'
 )
+parser.add_argument(
+    '-p',
+    '--playpause',
+    type=str,
+    metavar='play-pause indicator',
+    dest='play_pause'
+)
+
 args = parser.parse_args()
 
+def fix_string(string):
+    # corrects encoding for the python version used
+    if sys.version_info.major == 3:
+        return string
+    else:
+        return string.encode('utf-8')
+
 # Default parameters
-output = '{artist}: {song}'
+output = fix_string(u'{play_pause} {artist}: {song}')
 trunclen = 25
+play_pause = fix_string(u'\u25B6,\u23F8') # first character is play, second is paused
 
 # parameters can be overwritten by args
 if args.trunclen is not None:
     trunclen = args.trunclen
 if args.custom_format is not None:
     output = args.custom_format
+if args.play_pause is not None:
+    play_pause = args.play_pause
 
 try:
     session_bus = dbus.SessionBus()
@@ -45,9 +62,19 @@ try:
     )
 
     metadata = spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
+    status = spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus')
 
-    artist = metadata['xesam:artist'][0]
-    song = metadata['xesam:title']
+    play_pause = play_pause.split(',')
+
+    if status == 'Playing':
+        play_pause = play_pause[0]
+    elif status == 'Paused':
+        play_pause = play_pause[1]
+    else:
+        play_pause = str()
+
+    artist = fix_string(metadata['xesam:artist'][0])
+    song = fix_string(metadata['xesam:title'])
 
     if len(song) > trunclen:
         song = song[0:trunclen]
@@ -55,14 +82,10 @@ try:
         if ('(' in song) and (')' not in song):
             song += ')'
     
-    # Python3 uses UTF-8 by default. 
-    if sys.version_info.major == 3:
-        print(output.format(artist=artist, song=song))
-    else:
-        print(output.format(artist=artist, song=song).encode('UTF-8'))
+    print(output.format(artist=artist, song=song, play_pause=play_pause))
+
 except Exception as e:
     if isinstance(e, dbus.exceptions.DBusException):
         print('')
     else:
         print(e)
-
