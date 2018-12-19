c2e2116051e3b004dbba49e000296a40db70e168
SeraphyBR <luisjuniorbr@gmail.com>
2018-12-19 15:47:22 -0200

    Update polybar launch

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,15 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
 
 # Launch bar1 and bar2
-MONITOR=eDP-1 polybar seraphybr -r &
-#MONITOR=HDMI1 polybar seraphybr -r &
-MONITOR=eDP-1 polybar seraphybr_bottom -r &
-#MONITOR=HDMI1 polybar seraphybr_bottom -r &
+for monitor in $(polybar --list-monitors | cut -d ":" -f1); do
+    MONITOR=$monitor polybar seraphybr --reload --quiet &
+    MONITOR=$monitor polybar seraphybr_bottom --reload --quiet &
+done 
+
 echo "Bars launched..."
 dunstify -u low  "Bars launched"
+
