6f68241b3295c6e2927999d48a393380a22142a6
SeraphyBR <luisjuniorbr@gmail.com>
2019-01-25 17:23:35 -0200

    Update

--- Scripts/screenshot.sh
+++ Scripts/screenshot.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env sh
 
+tmp="$HOME/$(date +%F_%T).png"
 filename="$HOME/Imagens/Screenshots/$(date +%F_%T).png"
 
 if [ "$1" = "-s" ]
 then
-    maim --select -d 1 --bordersize 6 --color=0.23,0.70,0.30,0.8  "$filename"
+    maim --select -d 1 --bordersize 6 --color=0.23,0.70,0.30,0.8  "$tmp"
 else
-    maim -d 1 "$filename"
+    maim -d 1 "$tmp"
 fi
 
-notify-send -i "$filename"  "Screenshot" "Capture saved to '$filename'"
-
-xclip -selection clipboard -t image/png "$filename"
+if mv "$tmp" "$filename" 
+then
+    notify-send -i "$tmp"  "Screenshot" "Capture saved to '$filename'"
+    xclip -selection clipboard -t image/png "$filename"
+else
+    notify-send -i "$tmp"  "Screenshot" "Capture saved to '$tmp'"
+    xclip -selection clipboard -t image/png "$tmp" 
+fi
