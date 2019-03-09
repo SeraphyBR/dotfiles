7dd0807640b25e36d49e5b4fe2af716166c47731
SeraphyBR <luisjuniorbr@gmail.com>
2019-03-08 21:56:25 -0300

    Keybind to show keybindins

---
+++ Scripts/rofi-sxhkd.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+
+awk '/^[a-z]/ && last {print $0,"\t",last} {last=""} /^#/{last=$0}' ~/.config/sxhkd/sxhkdrc | column -t -s $'\t' | rofi -dmenu -scroll-method 1 -width 70 -p "Keybindings: "
