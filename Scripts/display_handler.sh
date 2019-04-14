c66e82ae1e09d0cc8da629421a7b85cf05be8166
SeraphyBR <luisjuniorbr@gmail.com>
2019-04-14 15:35:38 -0300

    Update

--- Scripts/display_handler.sh
+++ Scripts/display_handler.sh
@@ -8,9 +8,10 @@
         "HDMI-1 connected") 
             bspc monitor HDMI-1  -d I II III IV V 
             bspc monitor eDP-1 -d VI VII VIII IX X 
-            xrandr --output HDMI-1  --primary --auto --right-of eDP-1
+            xrandr --output HDMI-1  --primary --auto --left-of eDP-1
             ~/.config/polybar/launch.sh
-            feh --bg-tile ~/DotFiles/Images/Wallpapers/road_trees_top_view_119030_1920x1080.jpg
+            feh --bg-scale ~/DotFiles/Images/Wallpapers/fern_leaf_plant_green_119970_1920x1080.jpg \
+                ~/DotFiles/Images/Wallpapers/fern_leaf_plant_green_119970_1920x1080.jpg
             ;;
         "HDMI-1 disconnected")
             xrandr --output HDMI-1 --off
