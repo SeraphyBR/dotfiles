12ed4694d893cfdaf6079b0983f8b81194fe449e
SeraphyBR <luisjuniorbr@gmail.com>
2019-05-06 15:38:33 -0300

    remove herb

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,19 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-bspwm="$HOME/.config/polybar/config.bspwm"
-herbstluftwm="$HOME/.config/polybar/config.herbstluftwm"
-config="$HOME/.config/polybar/config"
-
-case "$1" in
-    bspwm) config="$bspwm" ;;
-    herb) config="$herbstluftwm" ;;
-esac
-
 # Launch bar1 and bar2
 for monitor in $(polybar --list-monitors | cut -d ":" -f1); do
-    MONITOR=$monitor polybar seraphybr -c "$config" --reload --quiet &
-    MONITOR=$monitor polybar seraphybr_bottom -c "$config" --reload --quiet &
+    MONITOR=$monitor polybar seraphybr --reload --quiet &
+    MONITOR=$monitor polybar seraphybr_bottom --reload --quiet &
 done 
 
 echo "Bars launched..."
