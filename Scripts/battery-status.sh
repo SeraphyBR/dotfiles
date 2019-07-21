72215d7d59c13dfb5247ec66836829c13c508c43
SeraphyBR <luisjuniorbr@gmail.com>
2019-07-20 23:23:18 -0300

    Update

--- Scripts/battery-status.sh
+++ Scripts/battery-status.sh
@@ -1,26 +1,25 @@
 #!/bin/sh
 
-ICON=/usr/share/icons/Papirus/48x48/devices/battery.svg
+ICON="/usr/share/icons/Papirus/48x48/devices/battery.svg"
+BAT="/sys/class/power_supply/BAT0"
 
 while true
 do
-    battery_level=$(acpi -b | grep -P -o '[0-9]+(?=%)')
-    if on_ac_power; then
+    LEVEL=$(cat "$BAT"/capacity)
+    STATUS=$(cat "$BAT"/status)
+    if [ "$STATUS" = "Charging" ]; then
 
-        if [ "$battery_level" -eq 100 ]; then
+        if [ "$LEVEL" -eq 100 ]; then
             notify-send -i $ICON "Bateria está carrega!" "Pode remover da tomada!"
             sleep 120
-        elif [ "$battery_level" -ge 85 ]; then
+        elif [ "$LEVEL" -ge 85 ]; then
             notify-send -i $ICON "Bateria está carregando acima de 80%!" "Por favor tire-o da tomada!"
             sleep 120
         fi
     else
-        if [ "$battery_level" -le 15 ]; then
+        if [ "$LEVEL" -le 15 ]; then
             notify-send -u critical -i $ICON "Bateria está abaixo de 15%!" "Precisa carregar! Por favor plugue o na tomada!."
             sleep 20
-            if ! on_ac_power; then
-                xbacklight -set 10
-            fi
         fi
     fi
 
