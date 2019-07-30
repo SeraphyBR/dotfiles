1530785005e9b6eb7c2cc521f27a61123bf5d2fe
SeraphyBR <luisjuniorbr@gmail.com>
2019-07-30 15:17:32 -0300

    Update

--- Scripts/mpd_notify.sh
+++ Scripts/mpd_notify.sh
@@ -1,17 +1,17 @@
-#! /bin/sh
+#!/bin/sh
 
 while true
 do
-    mpc idle player 
+    mpc idle player > /dev/null 2>&1
     NOTIFY_TITLE="Now Playing"
-    MUSIC=$(mpc current -f "%title%\\n%artist% - %album%")
-    FILE=$(mpc status -f '%file%' | head -n1)
+    MUSIC_INFO=$(mpc current -f "%title%\\n%artist% - %album%")
+    FILE=$(mpc current -f "%file%")
     DIR="$HOME/Música/$FILE"
     if ffmpeg -nostats -loglevel 0 -y -i "$DIR" /tmp/cover.jpg
-    then 
+    then
         IMG="/tmp/cover.jpg"
     else
         IMG="/usr/share/icons/Papirus/48x48/categories/audio-player.svg"
     fi
-    dunstify -u low -t 5000 -a MPD -r 1337 -i "$IMG" "$NOTIFY_TITLE"  "$MUSIC"
+    dunstify -u low -t 5000 -a MPD -r 1337 -i "$IMG" "$NOTIFY_TITLE"  "$MUSIC_INFO"
 done
