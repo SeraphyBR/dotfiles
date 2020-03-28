dad2fecfc2d990ef06cb69527d5af8ba0b8f30a1
SeraphyBR <luisjuniorbr@gmail.com>
2020-03-27 22:43:47 -0300

    Update

--- Scripts/record.sh
+++ Scripts/record.sh
@@ -1,29 +1,31 @@
 #!/bin/sh
+filename="$HOME/Vídeos/ScreenRecords/record_$(date +%Y-%m-%d_%H:%M:%S).mkv"
+tmpfile="$HOME/current_record_$(date +Y-%m-%d).mkv"
 
-tmp="/tmp/record_$(date +%Y-%m-%d).mkv"
-filename="$HOME/Vídeos/ScreenRecords/record_$(date +%Y-%m-%d_%H:%M).mkv"
+thumb="/tmp/thumb.jpg"
 
 width="1920"
 height="1080"
 
-x="0"
-y="0"
+xoffset="0"
+yoffset="0"
 
 # pactl list sources
 audio_card="alsa_output.pci-0000_00_1f.3.analog-stereo.monitor"
 
 if pgrep ffmpeg; then
     killall ffmpeg
-    notify-send "FFmpeg" "Gravação da tela finalizada!"
-    mv "$tmp" "$filename"
+    ffmpeg -y -nostats -loglevel 0 -i "$tmpfile" -an -frames:v 1 -ss 3 "$thumb"
+    dunstify -I "$thumb" "FFmpeg" "Gravação da tela finalizada!"
+    mv "$tmpfile" "$filename"
     exit 0
 fi
 
-notify-send "FFmpeg" "Gravação da tela iniciada!"
+dunstify "FFmpeg" "Gravação da tela iniciada!"
 
 ffmpeg -nostats -loglevel 0 -s ${width}x${height} \
 -f x11grab \
--i :0.0+${x},${y} \
+-i :0.0+${xoffset},${yoffset} \
 -f pulse \
 -ac 2 \
 -i "$audio_card" \
@@ -32,5 +34,5 @@ ffmpeg -nostats -loglevel 0 -s ${width}x${height} \
 -r 30 \
 -c:a libopus \
 -b:a 72k \
--y "$tmp" &
+-y "$tmpfile" &
 
